3437af4fefd92662017e23c2733680c7a4564e00
arthursens <arthursens2005@gmail.com>
2020-11-09 12:09:52 -0300

    bugfix: grafana deployment now load configmaps correctly
    Signed-off-by: arthursens <arthursens2005@gmail.com>

--- hack/install-grafana.sh
+++ hack/install-grafana.sh
@@ -5,5 +5,9 @@ if [[ $? == 0 ]]; then
   kubectl delete configmap kubevirt-provisioning
 fi
 
-kubectl create configmap kubevirt-provisioning --from-file grafana/dashboards/kubevirt-components.json --from-file grafana/dashboards/kubevirt-vmis.json
+kubectl create configmap kubevirt-provisioning \
+	--from-file grafana/provisioning/dashboards/kubevirt-components.json \
+	--from-file grafana/provisioning/dashboards/kubevirt-vmis.json \
+	--from-file grafana/provisioning/dashboards-provisioning.yml \
+	--from-file grafana/provisioning/datasources-provisioning.yml
 kubectl apply -f grafana/
